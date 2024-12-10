85bf96348bae6b9ec85f4d9457cf2f1e3b8c4e6a
Harshavardhan Kona <harshavardhan.kona06@icloud.com>
2024-12-10 12:21:18 -0500

    Add theme support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import React from "react";
 import "../styles/globals.css";
 import { spaceGrotesk, workSans, oxanium } from "@/lib/font";
+import {ThemeProvider} from "@/app/theme-provider";
 
 export const metadata: Metadata = {
   title: "Astrea Jobs | Navigate Your Career",
@@ -11,10 +12,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }
                                    : Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en" className={`${spaceGrotesk.variable} ${workSans.variable} ${oxanium.variable} antialiased`}>
-      <body>
-        {children}
+      <html lang="en" className={`${spaceGrotesk.variable} ${workSans.variable} ${oxanium.variable} antialiased`}
+            suppressHydrationWarning>
+      <body className="bg-background-light text-text-primary-light dark:bg-background-dark dark:text-text-primary-dark font-work-sans">
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
-    </html>
+      </html>
   );
 }
